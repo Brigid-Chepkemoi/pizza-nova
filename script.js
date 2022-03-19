a5bfc46fe40b4aa586b7c48483b97e3f5f2b0f39
brie <briememo95@gmail.com>
2022-03-20 00:52:47 +0300

    add prototypes,constructors

--- script.js
+++ script.js
@@ -5,23 +5,39 @@ $(document).ready(function(){
         var PizzaNumber=$("input#numorder").val()
         var MyPizza=$("#pizza").find(":selected").text()
         var MyTopping=$("#crust").find(":selected").text()
+        var PizzaSize=$("#pizzasize").find(":selected").text()
         console.log(MyName)
         console.log(PizzaNumber)
         console.log(MyPizza)
         console.log(MyTopping)
+        console.log(PizzaSize)
         const order1 = new MyFunction (MyName,PizzaNumber,MyPizza,MyTopping)
         console.log(order1)
     })
 
 
-function MyFunction(MyName,PizzaNumber,MyPizza,MyTopping){
+function MyFunction(MyName,PizzaNumber,MyPizza,MyTopping,PizzaSize){
 this.MyName=MyName
 this.PizzaNumber=PizzaNumber
 this.MyPizza=MyPizza
 this.MyTopping=MyTopping
+this.PizzaSize=PizzaSize
+
 
 }
 MyFunction.prototype.TotalPrice = function(){
+var SizePrice;
+if(this.PizzaSize === "Small"){
+SizePrice=500
+}
+else if(this.PizzaSize === "Medium"){
+    SizePrice=700
+}
+else{
+    SizePrice=1000
+}
+
+
 console.log("Total Price")
 }
 
