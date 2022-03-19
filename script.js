c2e7d15e77a89e2649c7a6fe2921b6e6ac753098
brie <briememo95@gmail.com>
2022-03-20 00:20:33 +0300

    add funtions

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-alert("Hey this works")
+    $("form#order").submit(function(){
+        event.preventDefault();
+        var MyName=$("input#name").val()
+        var PizzaNumber=$("input#numorder").val()
+        var MyPizza=$("#pizza").find(":selected").text()
+        var MyTopping=$("#crust").find(":selected").text()
+        console.log(MyName)
+        console.log(PizzaNumber)
+        console.log(MyPizza)
+        console.log(MyTopping)
+        const order1 = new MyFunction (MyName,PizzaNumber,MyPizza,MyTopping)
+        console.log(order1)
+    })
+
+
+function MyFunction(MyName,PizzaNumber,MyPizza,MyTopping){
+this.MyName=MyName
+this.PizzaNumber=PizzaNumber
+this.MyPizza=MyPizza
+this.MyTopping=MyTopping
+
+}
+MyFunction.prototype.TotalPrice = function(){
+console.log("Total Price")
+}
+
+
+
+
+
 });
