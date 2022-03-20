18d4c6bcbc78c01cfef20eafdde4b15f34f1c677
brie <briememo95@gmail.com>
2022-03-21 01:31:43 +0300

    add total price

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
 $(document).ready(function(){
-    $("form#order").submit(function(){
-        event.preventDefault();
+    var OrderLocation;
+    var total;
+    $("#orderbutton").click(function(event){
+        event.preventDefault(); 
         var MyName=$("input#name").val()
         var PizzaNumber=$("input#numorder").val()
         var MyPizza=$("#pizza").find(":selected").text()
         var MyTopping=$("#crust").find(":selected").text()
         var PizzaSize=$("#pizzasize").find(":selected").text()
-        var location=$("input#location").val()
-    
-        console.log(MyName)
-        console.log(PizzaNumber)
-        console.log(MyPizza)
-        console.log(MyTopping)
-        console.log(PizzaSize)
-        console.log(location) 
+        OrderLocation=$("input#location").val()
+        // console.log(MyName)
+        // console.log(PizzaNumber)
+        // console.log(MyPizza)
+        // console.log(MyTopping)
+        // console.log(PizzaSize)
+        // console.log(location) 
         
-        const order1 = new MyFunction (MyName,PizzaNumber,MyPizza,MyTopping)
+        const order1 = new MyFunction (MyName,PizzaNumber,MyPizza,MyTopping,PizzaSize,OrderLocation)
         console.log(order1)
-    })
+  order1.TotalPrice()
 
-
-function MyFunction(MyName,PizzaNumber,MyPizza,MyTopping,PizzaSize){
+    });
+function MyFunction(MyName,PizzaNumber,MyPizza,MyTopping,PizzaSize,OrderLocation){
 this.MyName=MyName
 this.PizzaNumber=PizzaNumber
 this.MyPizza=MyPizza
 this.MyTopping=MyTopping
 this.PizzaSize=PizzaSize
-
+this.OrderLocation=OrderLocation
 
 }
 MyFunction.prototype.TotalPrice = function(){
+    // console.log(this.MyName)
+
 var SizePrice;
-var OrdersNumber;
+var OrdersNumber=this.PizzaNumber;
+console.log(OrdersNumber)
+total=0
 if(this.PizzaSize === "Small"){
-SizePrice=500
-}
-else if(this.PizzaSize === "Medium"){
-    SizePrice=700
-}
-else{
-    SizePrice=1000
-}
-$("form#").submit(function(){
-var OrderDelivery;
+    SizePrice=500
+    }
+    else if(this.PizzaSize === "Medium"){
+            SizePrice=700
+        }
+        else{
+                SizePrice=1000
+            }
+            console.log(SizePrice)
+            total=SizePrice * OrdersNumber
+            console.log(total)
+            return total;
 
-});
-// if(this.OrderDelivery)
+        }; 
+   
+        $("#delivery").click(function(event){
+            event.preventDefault()
+            OrderLocation=$("input#location").val()
+            console.log(OrderLocation)
+            var deliveryFee=200;
+            var grandTotal;
+            grandTotal=total+ deliveryFee;
+    //    order1.total()
+       console.log(grandTotal);
+    }); 
+});   
+// console.log(SizePrice)
 
-// console.log("Total Price")
+// console.log(SizePrice)
+// var OrdersNumber= $("input#numorder").val();
+// console.log(OrdersNumber)
+//
+//
+//
+// let
+
 // }
-
-// Pick up notes
-// left at where to declare the delivery parameters 
-// how to calculate with the number of order chosen
-// how to write a function for the delivery submit button
-
-}
-
-});
+// });
